6337275c7c6c53d5c2b27781ff826892e3225d30
gousia <sgousia2000@gmail.com>
2022-03-21 19:21:28 +0530

    project

---
+++ Main.java
@@ -0,0 +1,15 @@
+package gousia;
+
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\GOUSIA\\Desktop\\LockedMe";
+	public static void main(String[] args) {
+		objectives menu = new objectives();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+	
+
+}
