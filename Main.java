42dce481c0ae05b89bb1532bf183826c4ca99b5f
gousia <sgousia2000@gmail.com>
2022-03-22 23:38:56 +0530

    project

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package gousia;
 public class Main {
 
 	/*Enter your desired Directory path */
-	public static final String path = "C:\\Users\\GOUSIA\\Desktop\\LockedMe";
+	public static final String path = "C:\\Users\\GOUSIA\\Desktop\\locked-me";
 	public static void main(String[] args) {
 		objectives menu = new objectives();
 		menu.introScreen();
--- project.java
+++ project.java
@@ -104,42 +104,42 @@ public void deleteFile(String path , String fileName) throws IOException {
 
 public void searchFile(String path , String fileName){
 
-		if (path == null || path.isEmpty() || path.isBlank())
-			throw new NullPointerException("Path cannot be Empty or null");
+	if (path == null || path.isEmpty() || path.isBlank())
+		throw new NullPointerException("Path cannot be Empty or null");
 
 
-		if (fileName == null || fileName.isEmpty() || fileName.isBlank())
-			throw new NullPointerException("File Name cannot be Empty or null");
+	if (fileName == null || fileName.isEmpty() || fileName.isBlank())
+		throw new NullPointerException("File Name cannot be Empty or null");
 
-		File dir = new File(path);
+	File dir = new File(path);
 
-		if(!dir.exists())
-			throw new IllegalArgumentException("Path does not exist");
+	if(!dir.exists())
+		throw new IllegalArgumentException("Path does not exist");
 
-		if(dir.isFile())
-			throw new IllegalArgumentException("The given path is a file. A directory is expected.");
+	if(dir.isFile())
+		throw new IllegalArgumentException("The given path is a file. A directory is expected.");
 
 
-		String [] fileList = dir.list();
-		boolean flag = false;
+	String [] fileList = dir.list();
+	boolean flag = false;
 
-		Pattern pat = Pattern.compile(fileName);
+	Pattern pat = Pattern.compile(fileName);
 
-		if(fileList != null && fileList.length > 0) {
-			for(String file:fileList) {
-				Matcher mat = pat.matcher(file);
-				if(mat.matches()) {
-					System.out.println("File Found at location: " + dir.getAbsolutePath());
-					flag = true;
-					break;
-				}
+	if(fileList != null && fileList.length > 0) {
+		for(String file:fileList) {
+			Matcher mat = pat.matcher(file);
+			if(mat.matches()) {
+				System.out.println("File Found at location: " + dir.getAbsolutePath());
+				flag = true;
+				break;
 			}
 		}
-
-		if(flag == false)
-			System.out.println("File Not Found.. Please try again.");
-
-
 	}
+
+	if(flag == false)
+		System.out.println("File Not Found.. Please try again.");
+
+
+}
 
 }
